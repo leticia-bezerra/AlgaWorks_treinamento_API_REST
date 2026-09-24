81d4e455457f4b2f2dfcc4c1f293587036817574
leticia-bezerra <leticia21bezerradearaujo@gmail.com>
2026-09-23 21:47:19 -0300

    feat: criaçao endpoint de inclusao de veiculos

---
+++ src/main/java/com/algaworks/algatransito/domain/service/RegistroVeiculoService.java
@@ -0,0 +1,25 @@
+package com.algaworks.algatransito.domain.service;
+
+import com.algaworks.algatransito.domain.model.StatusVeiculo;
+import com.algaworks.algatransito.domain.model.Veiculo;
+import com.algaworks.algatransito.domain.repository.VeiculoRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@Service
+public class RegistroVeiculoService {
+
+    private VeiculoRepository veiculoRepository;
+
+    @Transactional
+    public Veiculo cadastrar(Veiculo novoVeiculo){
+        novoVeiculo.setStatus(StatusVeiculo.APREENDIDO);
+        novoVeiculo.setDataCadastro(LocalDateTime.now());
+
+        return veiculoRepository.save(novoVeiculo);
+    }
+}
